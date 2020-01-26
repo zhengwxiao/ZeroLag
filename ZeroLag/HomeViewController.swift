9ac467594a13d7ca744e6da82f711c8daf3fe789
Zheng Xiao <zhengwxiao@tamu.edu>
2020-01-26 16:54:15 -0600

    Fixed constraints bugs

--- ZeroLag/HomeViewController.swift
+++ ZeroLag/HomeViewController.swift
@@ -14,6 +14,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.navigationItem.setHidesBackButton(true, animated: true);
     }
     
 
