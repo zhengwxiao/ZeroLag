87dc919f6807d592f98165e49b9a998962d2fba2
jks5464 <justinkasiu@gmail.com>
2020-01-26 03:10:58 -0600

    Made Json parser

--- ZeroLag/AddFlightViewController.swift
+++ ZeroLag/AddFlightViewController.swift
@@ -8,10 +8,61 @@
 
 import UIKit
 
+struct Flight: Decodable {
+    let flightNumber: String
+    let origin: origin
+    let destination: destination
+    let distance: Int
+    let duration: duration
+    let departureTime: String
+    let arrivalTime: String
+    let aircraft: aircraft
+}
+
+struct origin: Decodable {
+    let code: String
+    let city: String
+    let timezone: String
+    let location: location
+}
+
+struct destination: Decodable {
+    let code: String
+    let city: String
+    let timezone: String
+    let location: location
+}
+
+struct duration: Decodable {
+    let locale: String
+    let hours: Int
+    let minutes: Int
+}
+
+struct aircraft: Decodable {
+    let model: String
+    let passengerCapacity: passengerCapacity
+    let speed: Int
+}
+
+struct location: Decodable {
+    let latitude: Double
+    let longitude: Double
+}
+
+struct passengerCapacity: Decodable {
+    let total: Int
+    let main: Int
+    let first: Int
+}
+
 class AddFlightViewController: UIViewController {
     
     @IBOutlet weak var flightDatePicker: UIDatePicker!
     @IBOutlet weak var flightNumberTextField: UITextField!
+    var date = ""
+    var flightNumber = ""
+    var choice: Flight?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +74,37 @@ class AddFlightViewController: UIViewController {
     @IBAction func searchPressed(_ sender: Any) {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        print(formatter.string(from: flightDatePicker.date))
-        print(flightNumberTextField.text!)
+        date = formatter.string(from: flightDatePicker.date)
+        flightNumber = flightNumberTextField.text!
+        print(date)
+        print(flightNumber)
+        getFlight()
     }
     
+    func getFlight() {
+        let jsonUrlString = "https://noiseout.herokuapp.com/flights?date=\(date)"
+        guard let url = URL(string: jsonUrlString) else
+            {return}
+        
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            guard let data = data else {return}
+           // let dataAsString = String(data: data, encoding: .utf8)
+           // print(dataAsString)
+            do {
+                let flightData = try JSONDecoder().decode([Flight].self, from: data)
+                for flight in flightData {
+                    if flight.flightNumber == self.flightNumber {
+                        self.choice = flight
+                        print(flight)
+                    }
+                }
+            } catch let JsonErr {
+                print("Error Serializing json:", JsonErr)
+            }
+            
+        }.resume()
+    }
     
     /*
     // MARK: - Navigation
