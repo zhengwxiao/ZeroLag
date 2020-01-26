ace49bf0f1c48595448e760d6d156e0f6347d8f7
Zheng Xiao <zhengwxiao@tamu.edu>
2020-01-26 04:22:26 -0600

    Added setup views

---
+++ ZeroLag/Setup/SleepScheduleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SleepScheduleViewController.swift
+//  ZeroLag
+//
+//  Created by Z Xiao on 1/26/20.
+//  Copyright © 2020 Z Xiao. All rights reserved.
+//
+
+import UIKit
+
+class SleepScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
