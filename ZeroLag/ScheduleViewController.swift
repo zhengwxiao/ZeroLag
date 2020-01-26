37ced06ff5f30a865203a05a186f60e35d36312e
Zheng Xiao <zhengwxiao@tamu.edu>
2020-01-26 03:02:45 -0600

    Added sleep countdown label

--- ZeroLag/ScheduleViewController.swift
+++ ZeroLag/ScheduleViewController.swift
@@ -11,22 +11,34 @@ import CountdownLabel
 
 class ScheduleViewController: UIViewController {
     
-    @IBOutlet weak var waterReminderLabel: CountdownLabel!
+    @IBOutlet weak var waterCountdownLabel: CountdownLabel!
+    
+    @IBOutlet weak var sleepCountdownLabel: CountdownLabel!
+    
     
     let notificationController = NotificationController()
+    let customBlue = UIColor(red: 0.029, green: 0.269, blue: 0.629, alpha: 0.58)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        waterReminderLabel.countdownDelegate = self
-        waterReminderLabel.textColor = .white
-        waterReminderLabel.font = UIFont(name:"Helvetica", size: 30)
-        waterReminderLabel.setCountDownTime(minutes: 3) // Input in seconds
-        waterReminderLabel.then(targetTime: 1) { [unowned self] in
+        waterCountdownLabel.textColor = customBlue
+        waterCountdownLabel.font = UIFont(name:"Futura-Bold", size: 25)
+        waterCountdownLabel.setCountDownTime(minutes: 10) // Input in seconds
+        waterCountdownLabel.then(targetTime: 1) { [unowned self] in
             self.notificationController.scheduleReminders(title: "Drink Water", body: "Hydrate or diedrate", delay: 1)
         }
-        waterReminderLabel.start()
+        waterCountdownLabel.start()
+        
+        sleepCountdownLabel.textColor = .systemOrange
+        sleepCountdownLabel.font = UIFont(name:"Futura-Bold", size: 25)
+        sleepCountdownLabel.setCountDownTime(minutes: 20) // Input in seconds
+        sleepCountdownLabel.then(targetTime: 1) { [unowned self] in
+            self.notificationController.scheduleReminders(title: "Time to Sleep", body: "Try to sleep now to beat jet lag!", delay: 1)
+        }
+        sleepCountdownLabel.start()
+        
     }
 }
 
